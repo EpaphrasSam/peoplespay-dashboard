237fee74607ede682d0d2fb9b237b5046116a770
codeviktim <johnquarcoopome.jnr@gmail.com>
2021-12-02 11:51:47 +0000

    https fix

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
-const PRODUCTION : boolean = process.env.NODE_ENV === 'production'
-const url: string = PRODUCTION ?
-    'http://3.15.51.144/peoplepay'
+const production : boolean = process.env.NODE_ENV === 'production'
+const url: string = production ?
+    'https://3.15.51.144/peoplepay'
     :
     'http://3.15.51.144/peoplepay'
     //'http://3.135.217.52/peoplepay';
